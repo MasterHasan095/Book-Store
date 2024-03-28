7300b9d4151d0c74735e0f22e5bd98bbdfa199db
masterha <masterha@sheridancollege.ca>
2024-03-28 18:35:50 -0400

    Implemented Delete and Edit

--- frontend/book-store/src/components/pages/DeleteBook.jsx
+++ frontend/book-store/src/components/pages/DeleteBook.jsx
@@ -1,4 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../Spinner";
+import { useNavigate } from "react-router-dom";
+
+
 const DeleteBook = () =>{
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        setLoading(true);
+        axios
+          .delete(`http://localhost:5555/books/${id}`)
+          .then((response) => {
+            setLoading(false);
+            handleDeleteBook();
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+       
+      }, []);
+
+      const handleDeleteBook =()=>{
+        navigate("/");
+      }
     return (
         <h1>
             Delete
